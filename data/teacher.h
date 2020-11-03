425a11fbb7315e76765c878272f664e4f563e114
Marko Yaciv <2002mar4ik2002@gmail.com>
2020-11-03 22:26:58 +0200

    added initials for teacher class

--- data/teacher.h
+++ data/teacher.h
@@ -1,9 +1,16 @@
 #ifndef TEACHER_H
 #define TEACHER_H
-
+#include <QString>
 
 class Teacher
 {
+
+    struct Initials{
+        QString fName;
+        QString lName;
+        QString fthName;
+    }inits;
+
 public:
     Teacher();
 };
